7251ca15511c4e6a95511dc3f1df92093cb200cd
VictorHarvey2207 <150213985+VictorHarvey2207@users.noreply.github.com>
2024-12-19 00:47:51 +0700

    Add files via upload

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,18 +1,58 @@
-package hust.soict.cybersecurity.aims.store;
+package hust.soict.dsai.aims.store;
+
 import java.util.ArrayList;
-import hust.soict.cybersecurity.aims.media.*;
+
+import hust.soict.dsai.aims.media.Media;
 
 public class Store {
-	private ArrayList<Media> itemsInStore = new ArrayList<Media>();
-	
-	public void addMedia(Media media) {
-		itemsInStore.add(media);
+    private ArrayList<Media> itemsInStore;
+
+    public Store() {
+        itemsInStore = new ArrayList<Media>();
+    }
+
+	public ArrayList<Media> getItemsInStore() {
+		return itemsInStore;
 	}
-	
-	public void removeMedia(Media media) {
-		itemsInStore.remove(media);
+
+    public void displayItems() {
+		for (Media item: itemsInStore) {
+			System.out.println(item.toString());
+		}
 	}
-	
+
+    public void addMedia(Media media) {
+        if (media != null) {
+            itemsInStore.add(media);
+        } else {
+            System.out.println("Can not add null item!");
+        }
+    }
+
+    public void removeMedia(Media media) {
+    	if (itemsInStore.contains(media)) {
+    		itemsInStore.remove(media);
+    	} else {
+    		System.out.println("This item is not in store!");
+    	}
+    }
+
+    public Media searchById(int id) {
+		boolean found = false;
+		for (int i = 0; i < itemsInStore.size(); i++) {
+			if (itemsInStore.get(i).getId() == id) {
+				System.out.println("Media found: " + itemsInStore.get(i).toString());
+				found = true;
+				return itemsInStore.get(i);
+			}
+		}
+
+		if (!found) {
+			System.out.println("No Media found with ID: " + id);
+		}
+        return null;
+	}
+
 	public Media searchByTitle(String title) {
 		boolean found = false;
 		for (int i = 0; i < itemsInStore.size(); i++) {
@@ -22,18 +62,10 @@ public class Store {
 				return itemsInStore.get(i);
 			}
 		}
+
 		if (!found) {
-			System.out.println("There are no medias that match your search");
+			System.out.println("No Media found with title: " + title);
 		}
-		return null;
-	}
-	
-	public void print() {
-		System.out.println("***********************STORE***********************");
-		System.out.println("Store:");
-		for (int i = 0; i < itemsInStore.size(); i++) {
-			System.out.println((i + 1) + ". DVD - " + itemsInStore.get(i).toString());
-		}
-		System.out.println("***************************************************");
+        return null;
 	}
 }
