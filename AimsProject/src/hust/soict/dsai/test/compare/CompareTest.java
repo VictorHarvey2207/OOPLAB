ee8f37f2910e4c6e85e8d16973ecc169c2dcac61
VictorHarvey2207 <150213985+VictorHarvey2207@users.noreply.github.com>
2024-12-19 00:50:23 +0700

    Add files via upload

--- AimsProject/src/hust/soict/dsai/test/compare/CompareTest.java
+++ AimsProject/src/hust/soict/dsai/test/compare/CompareTest.java
@@ -1,33 +1,41 @@
-package hust.soict.cybersecurity.test.compare;
+package hust.soict.dsai.test.compare;
 
-import hust.soict.cybersecurity.aims.cart.Cart;
-import hust.soict.cybersecurity.aims.media.Book;
-import hust.soict.cybersecurity.aims.media.CompactDisc;
-import hust.soict.cybersecurity.aims.media.DigitalVideoDisc;
-import hust.soict.cybersecurity.aims.media.Track;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
 public class CompareTest {
-	public static void main(String[] args) {
-		Cart cart = new Cart();
-		
-		CompactDisc cd = new CompactDisc("Pink Panther", "Cartoon", "Animator", 10.8f);
-		
-		cd.addTrack(new Track("Episode 1", 10));
-		cd.addTrack(new Track("Episode 2", 9));
-		
-		DigitalVideoDisc dvd = new DigitalVideoDisc("The Lion King", "Animation",
-				"Roger Allers", 87, 19.95f);
-		
-		Book book = new Book("HUST", "Education", 5.2f);
-		book.addAuthor("Author A");
-		book.addAuthor("Author B");
-		
-		cart.addMedia(cd);
-		cart.addMedia(dvd);
-		cart.addMedia(book);
-		
-		cart.sortByCostTitle();
-		System.out.println();
-		cart.sortByTitleCost();
-	}
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+		// create some media here
+		// for example: cd, dvd, book
+		CompactDisc cd1 = new CompactDisc("Son Tung MTP", "Album Special", "Classic", "Son Tung MTP", 24.55f);
+		CompactDisc cd2 = new CompactDisc("Amee");
+
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+				"Animation", "Roger Allers", 87, 19.95f);
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+				"Science Fiction", "George Lucas", 87, 24.95f);
+
+		Book b1 = new Book("1 Van Cau Hoi Vi sao", "Truyen", 80.25f);
+		Book b2 = new Book("So Dua", "Truyen Co Tich", 40.5f);
+
+//		cart.addMedia(cd1);
+//		cart.addMedia(cd2);
+//		cart.addMedia(dvd1);
+//		cart.addMedia(dvd2);
+//		cart.addMedia(b1);
+//		cart.addMedia(b2);
+
+        // Sort by Title, then Cost
+        cart.sortByTitleCost();
+
+        System.out.println("");
+
+        // Sort by Cost, then Title
+        cart.sortByCostTitle();
+    }
 }
